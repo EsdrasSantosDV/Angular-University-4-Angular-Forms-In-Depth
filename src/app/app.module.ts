8260d5167a68c3f71bff46315de592a5356e1ea0
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-20 23:05:00 -0300

    feat:ng model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {BrowserModule} from '@angular/platform-browser';
+ import {BrowserModule} from '@angular/platform-browser';
 import {forwardRef, NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
