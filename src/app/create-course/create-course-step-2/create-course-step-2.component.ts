f30ef3bce244e6fb4b18a17500012d09a1683452
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-27 16:43:42 -0300

    feat:disable field dynamic

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -10,8 +10,13 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class CreateCourseStep2Component implements OnInit {
 
   form=this.fb.group({
-    courseType:['premium',[Validators.required]]
-
+    courseType:['premium',[Validators.required]],
+    price:[null,[
+      Validators.required,
+      Validators.min(1),
+      Validators.max(9999),
+      Validators.pattern("[0-9]+")
+    ]]
   });
 
 
@@ -21,6 +26,20 @@ export class CreateCourseStep2Component implements OnInit {
 
   ngOnInit() {
 
+    //AQUI E PRA DESABILITAR O VALOR DO PREÇO QUANDO O COURSE E DO TIPO FREE!
+    this.form.valueChanges.subscribe(val=>{
+      const priceControl=this.form.controls['price'];
+      if(val.courseType=='free' && priceControl.enabled)
+      {
+        priceControl.disable({emitEvent:false});
+      }
+      else{
+        if(val.courseType=='premium' && priceControl.disabled)
+        {
+          priceControl.enable({emitEvent:false});
+        }
+      }
+    });
 
 
   }
